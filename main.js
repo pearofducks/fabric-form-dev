2a40333407eadc881a8e6354f36457e5f0c04c0d
Dave Honneffer <pearofducks@gmail.com>
2022-11-28 20:53:52 +0100

    Working

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ import './forms.css'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div class="radio"></div>
-  <div class="radio checked"></div>
+  <h4 style="text-align: left; margin-bottom: 8px;">Dead toggles</h4>
+  <div style="display: grid; grid-template-columns: 1fr 1fr;">
+    <div class="radio" style="pointer-events: none"></div>
+    <div class="radio checked" style="pointer-events: none"></div>
+  </div>
   <div style="margin: 32px 0;"></div>
   <label style="display: flex; align-items: center;">
     <input type="radio" class="radio" name="foo" />
@@ -16,8 +19,11 @@ document.querySelector('#app').innerHTML = `
   </label>
 
   <div style="margin: 32px 0;"></div>
-  <div class="checkbox"></div>
-  <div class="checkbox checked"></div>
+  <h4 style="text-align: left; margin-bottom: 8px;">Dead toggles</h4>
+  <div style="display: grid; grid-template-columns: 1fr 1fr;">
+    <div class="checkbox" style="pointer-events: none"></div>
+    <div class="checkbox checked" style="pointer-events: none"></div>
+  </div>
   <div style="margin: 32px 0;"></div>
   <label style="display: flex; align-items: center;">
     <input type="checkbox" class="checkbox" name="bar" />
@@ -28,6 +34,11 @@ document.querySelector('#app').innerHTML = `
     <span style="margin-left: 8px">i am a checkbox field</span>
   </label>
   </div>
+  <ol style="margin-top: 48px">
+    <li>display block ok for input el?</li>
+    <li>how do we size these? will it need to change per-font/per-brand?</li>
+    <li>more bold checkmark?</li>
+  </ol>
 `
 
 setupCounter(document.querySelector('#counter'))
