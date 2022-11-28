999d46464fc4d59b8d36471d3f469bbfa2e9e0ea
Dave Honneffer <pearofducks@gmail.com>
2022-11-28 16:46:46 +0100

    Yay

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 import './style.css'
 import './forms.css'
-import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div class="radio"></div>
   <div class="radio checked"></div>
-  <br/>
-  <label>
+  <div style="margin: 32px 0;"></div>
+  <label style="display: flex; align-items: center;">
     <input type="radio" class="radio" name="foo" />
-    <span>i am a radio field</span>
+    <span style="margin-left: 8px;">i am a radio field</span>
   </label>
-  <br />
-  <label style="margin-top: 8px; display: block;">
+  <label style="margin-top: 8px; display: flex; align-items: center;">
     <input type="radio" class="radio" name="foo" />
-    <span>i am a radio field</span>
+    <span style="margin-left: 8px">i am a radio field</span>
+  </label>
+
+  <div style="margin: 32px 0;"></div>
+  <div class="checkbox"></div>
+  <div class="checkbox checked"></div>
+  <div style="margin: 32px 0;"></div>
+  <label style="display: flex; align-items: center;">
+    <input type="checkbox" class="checkbox" name="bar" />
+    <span style="margin-left: 8px;">i am a checkbox field</span>
+  </label>
+  <label style="margin-top: 8px; display: flex; align-items: center;">
+    <input type="checkbox" class="checkbox" name="bar" />
+    <span style="margin-left: 8px">i am a checkbox field</span>
   </label>
   </div>
 `
