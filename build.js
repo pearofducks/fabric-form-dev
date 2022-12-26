2c77fc5127fad54120caaf96aaf2ae4decd8ca45
Dave Honneffer <pearofducks@gmail.com>
2022-12-26 22:44:15 +0100

    Get rid of nesting due to bug

--- build.js
+++ build.js
@@ -3,7 +3,6 @@ import * as lightning from 'lightningcss'
 const { code } = lightning.bundle({
   filename: 'forms.css',
   minify: true,
-  drafts: { nesting: true, customMedia: true },
   targets: {
     safari: (12 << 16),
     chrome: (80 << 16),
