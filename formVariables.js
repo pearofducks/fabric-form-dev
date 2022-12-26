7651f7fd33b64686a1e3d0dbfe724688273a462a
Dave Honneffer <pearofducks@gmail.com>
2022-12-26 14:53:27 +0100

    Cleanup

--- formVariables.js
+++ formVariables.js
@@ -2,7 +2,8 @@ import svgToDataUri from 'mini-svg-data-uri'
 
 const encode = svg => `url("${svgToDataUri(svg)}")`
 
-const selectChevron = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"><path stroke="#71717a" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6 8l4 4 4-4"/></svg>`
+// NB: we can't (easily) use a CSSvar for stroke here - need to just standardize on a color
+const selectChevron = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"><path stroke="#333" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6 8l4 4 4-4"/></svg>`
 const checkMark = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="white"><path d="M12.207 4.793a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-2-2a1 1 0 011.414-1.414L6.5 9.086l4.293-4.293a1 1 0 011.414 0z"/></svg>`
 const checkDash = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16"><path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h8"/></svg>`
 
